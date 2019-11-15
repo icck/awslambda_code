b87963676c408613406562f0474deef76f0b6857
icck <icck.n.n1111@gmail.com>
2019-11-15 18:57:32 +0900

    first!

---
+++ dynamo/get_person.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+
+dynamodb = boto3.resource('dynamodb')
+# table_name:icck-person pk:person_id sk: Other: name
+# dynamodb 作ってから実行
+table = dynamodb.Table('icck-person')
+
+def get_person(id):
+    respose = table.get_item(
+            Key={
+                'person_id':id
+            }
+        )
+    return respose['Item']
+
+
+def lambda_handler(event, context):
+    person = get_person('001')
+    return person
